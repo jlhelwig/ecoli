3da650fd294e6f0f5937785caf5ce3d001af2b08
jlhelwig <jeff.helwig@gmail.com>
2017-07-05 16:59:11 -0500

    fix a typo

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
  Twit = require("twit");
  TwitterBot = require("node-twitterbot").TwitterBot;
  Bot = new TwitterBot({
-  
+
   //for deployment in heroku
  consumer_key: process.env.BOT_CONSUMER_KEY,
  consumer_secret: process.env.BOT_CONSUMER_SECRET,
@@ -13,7 +13,7 @@ var phraseArray = [ "going swimming in a creek in Austin? Check for more info",
                     "tweet tweet--Austin TX--check the bacteria levels",
                     "tweetstorm? recent rainstorm? hmmmm....",
                     "looking forward to giving you a count, still in development!",
-                    "it me, Colin the fecal e.coli! check out my numbers"
+                    "it's me, Colin the fecal e.coli! check out my numbers"
                   ];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
