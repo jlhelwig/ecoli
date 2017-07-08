11fb1519a8cee32863be3b2481d53b9f8e2d963f
jlhelwig <jeff.helwig@gmail.com>
2017-07-08 15:04:29 -0500

    creates a temp file for sample_date, and fs to write and read the file

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// Today
+
 // require('locus')
 var fs = require("fs")
 require('dotenv').config()
 var Twit = require("twit");
 var TwitterBot = require("node-twitterbot").TwitterBot;
+
 var dateMax = new Date();
 var prevDate = new Date().setDate(dateMax.getDate()-90);
 dateMax = dateMax.toISOString();
 var dateMin = new Date(prevDate);
-var waterThing =[]
 dateMin = dateMin.toISOString();
 dateMin=dateMin.slice(0,-1);
 dateMax=dateMax.slice(0,-1);
+var waterThing =[]
 // eval(locus)
-console.log(dateMax,dateMin);
+// console.log(dateMax,dateMin);
 var uriThing = encodeURI(`https://data.austintexas.gov/resource/v7et-4fvp.json?$where=sample_date>='${dateMin}'and sample_date<='${dateMax}'and parameter='E COLI BACTERIA'`)
-console.log(uriThing);
+// console.log(uriThing);
 var request = require('request-promise-native');
 var options = {
     uri: uriThing,
@@ -27,7 +28,7 @@ var options = {
 };
 
 request(options)
-    .then(function (data) {
+    .then(function(data){
 
         waterThing = data
 
@@ -37,21 +38,41 @@ request(options)
             access_token: process.env.BOT_ACCESS_TOKEN,
             access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
           })
-          console.log(waterThing)
+           var latestDate=[]
             for (let i=0; i<waterThing.length; i++){
-              var shortDate = new Date(waterThing[i].sample_date).toLocaleDateString()
-              console.log(shortDate);
-              if (waterThing[i].result<125){
-                Bot.tweet(`E Coli count on ${shortDate} was ${waterThing[i].result}/100ml \u{2705} \u{1f3ca} at www.google.com/maps/place/${waterThing[i].lat_dd_wgs84},${waterThing[i].lon_dd_wgs84}`);
-                }
-              else {
-                Bot.tweet(`E Coli count on ${shortDate} was ${waterThing[i].result}/100ml \u{1f6ab} \u{1f4a9} at www.google.com/maps/place/${waterThing[i].lat_dd_wgs84},${waterThing[i].lon_dd_wgs84}`);
-
-              }
+              latestDate.push(waterThing[i].sample_date)
             }
-          // Bot.tweet("\u{2705}"+"\u{1f3ca}"+" or "+"\u{1f6ab}"+"\u{1f4a9}")
-    })
-//
-//     .catch(function (err) {
-//         console.log('here is the error: ',err);// API call failed...
-//     });
+              latestDate=latestDate.sort()
+              console.log('this is the last sample date ', latestDate[latestDate.length-1]);
+
+              // fs = require('fs');
+fs.writeFile('time.json', latestDate[latestDate.length-1], function (err) {
+  if (err) return console.log(err);
+  console.log('this should have sent something to time.json');
+});
+fs = require('fs')
+fs.readFile('time.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('this is what was in time.json',data);
+});
+
+            for (let i=0; i<waterThing.length; i++){
+
+              var shortDate = new Date(waterThing[i].sample_date).toLocaleDateString()
+              if (waterThing[i].result<125){
+
+                // Bot.tweet(`E Coli count on ${shortDate} was ${waterThing[i].result}/100ml \u{2705} \u{1f3ca} at www.google.com/maps/place/${waterThing[i].lat_dd_wgs84},${waterThing[i].lon_dd_wgs84}`);
+                }
+              else{
+
+                // Bot.tweet(`E Coli count on ${shortDate} was ${waterThing[i].result}/100ml \u{1f6ab} \u{1f4a9} at www.google.com/maps/place/${waterThing[i].lat_dd_wgs84},${waterThing[i].lon_dd_wgs84}`);
+                }
+              }
+
+console.log('this is the raw date from the returned object', latestDate);
+        })
+    .catch(function (err) {
+      console.log('here is the error: ',err);// API call failed...
+    });
